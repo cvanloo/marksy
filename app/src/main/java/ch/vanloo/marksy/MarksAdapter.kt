b0710bcf8275830fc2e3fd41989c8a24f16beb56
Miya <104875-miyaaaa@users.noreply.framagit.org>
2022-05-27 15:17:49 +0200

    ref: clean-up

--- app/src/main/java/ch/vanloo/marksy/MarksAdapter.kt
+++ app/src/main/java/ch/vanloo/marksy/MarksAdapter.kt
@@ -5,31 +5,26 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
-import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
 
 class MarksAdapter(private val context: Context, private var marks: List<Mark>) :
     RecyclerView.Adapter<MarksAdapter.ViewHolder>() {
 
-    class ViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         private var mark: Mark? = null
 
-        private val markName: TextView
-        private val markValue: TextView
-        private val markWeighting: TextView
-
-        init {
-            markName = view.findViewById(R.id.mark_name)
-            markValue = view.findViewById(R.id.mark_value)
-            markWeighting = view.findViewById(R.id.mark_weighting)
-        }
+        private val markName: TextView = view.findViewById(R.id.mark_name)
+        private val markValue: TextView = view.findViewById(R.id.mark_value)
+        private val markWeighting: TextView = view.findViewById(R.id.mark_weighting)
 
         fun bindMark(context: Context, mark: Mark) {
             this.mark = mark
+
             val (value, weighting, name) = mark
             markName.text = name
             markValue.text = value.toString()
             markWeighting.text = context.getString(R.string.mark_weighting, weighting)
+
             if (value == 6.0f) {
                 markValue.setTextColor(context.getColor(R.color.mark_6_0))
             } else if (value >= 5.5f) {
