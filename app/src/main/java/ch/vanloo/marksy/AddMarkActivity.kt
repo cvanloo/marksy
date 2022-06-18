8c1484746f0d477a79b6510c49d45de798712d85
Miya <104875-miyaaaa@users.noreply.framagit.org>
2022-06-18 14:39:40 +0200

    feat(date): Date picker dialog to select date

--- app/src/main/java/ch/vanloo/marksy/AddMarkActivity.kt
+++ app/src/main/java/ch/vanloo/marksy/AddMarkActivity.kt
@@ -1,13 +1,27 @@
 package ch.vanloo.marksy
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import ch.vanloo.marksy.databinding.ActivityAddMarkBinding
 import kotlinx.coroutines.launch
-import java.util.Date
+import java.util.*
 
 class AddMarkActivity : AppCompatActivity() {
     private lateinit var binding: ActivityAddMarkBinding
+
+    private var date: Long = 0
+
+    private val datePickerDialogListener =
+        DatePickerDialog.OnDateSetListener { _, year, month, dayOfMonth ->
+            val calendar = Calendar.getInstance()
+            calendar.set(year, month, dayOfMonth)
+            this.date = calendar.timeInMillis
+            binding.inputDate.setText(getString(R.string.formatted_date,
+                calendar.get(Calendar.DAY_OF_MONTH),
+                calendar.get(Calendar.MONTH),
+                calendar.get(Calendar.YEAR)))
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,11 +32,21 @@ class AddMarkActivity : AppCompatActivity() {
             finish()
         }
 
+        binding.inputDate.setOnClickListener {
+            val calendar = Calendar.getInstance()
+            DatePickerDialog(this,
+                datePickerDialogListener,
+                calendar.get(Calendar.YEAR),
+                calendar.get(Calendar.MONTH),
+                calendar.get(Calendar.DAY_OF_MONTH)
+            ).show()
+        }
+
         binding.buttonCreate.setOnClickListener {
             val value = binding.inputMark.text.toString().toFloat()
             val weighting = binding.inputWeighting.text.toString().toFloat()
             val name = binding.inputName.text.toString()
-            val date = Date()
+            val date = Date(date)
 
             val mark = Mark(0, value, weighting, name, date)
 
