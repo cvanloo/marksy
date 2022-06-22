5f5b5a018554578d860b67fc75f827fbefe871ea
Miya <104875-miyaaaa@users.noreply.framagit.org>
2022-06-22 19:55:51 +0200

    fix(mark_details): Update view on main thread

--- app/src/main/java/ch/vanloo/marksy/MarkDetailsActivity.kt
+++ app/src/main/java/ch/vanloo/marksy/MarkDetailsActivity.kt
@@ -7,9 +7,9 @@ import ch.vanloo.marksy.db.MarkDao
 import ch.vanloo.marksy.db.MarksDatabase
 import ch.vanloo.marksy.entity.Mark
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import java.text.DateFormat
-import java.util.*
 
 class MarkDetailsActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMarkDetailsBinding
@@ -31,26 +31,28 @@ class MarkDetailsActivity : AppCompatActivity() {
             val uid = intent.getLongExtra(MARK_ID, 0)
             mark = dao.getById(uid)
 
-            binding.preview.markValue.text = mark.Value.toString()
-            binding.preview.markWeighting.text = mark.Weighting.toString()
-            binding.preview.markName.text = mark.Name
+            launch(Dispatchers.Main) {
+                binding.preview.markValue.text = mark.Value.toString()
+                binding.preview.markWeighting.text = mark.Weighting.toString()
+                binding.preview.markName.text = mark.Name
 
-            val formattedDate = DateFormat.getDateInstance().format(mark.Date)
-            binding.preview.markDate.text = getString(R.string.formatted_date, formattedDate)
+                val formattedDate = DateFormat.getDateInstance().format(mark.Date)
+                binding.preview.markDate.text = getString(R.string.formatted_date, formattedDate)
 
-            // @DUP: Coloring:2
-            if (mark.Value == 6.0f) {
-                binding.preview.markValue.setTextColor(getColor(R.color.mark_6_0))
-            } else if (mark.Value >= 5.5f) {
-                binding.preview.markValue.setTextColor(getColor(R.color.mark_5_5))
-            } else if (mark.Value >= 5.0f) {
-                binding.preview.markValue.setTextColor(getColor(R.color.mark_5_0))
-            } else if (mark.Value >= 4.5f) {
-                binding.preview.markValue.setTextColor(getColor(R.color.mark_4_5))
-            } else if (mark.Value >= 4.0f) {
-                binding.preview.markValue.setTextColor(getColor(R.color.mark_4_0))
-            } else {
-                binding.preview.markValue.setTextColor(getColor(R.color.mark_failing))
+                // @DUP: Coloring:2
+                if (mark.Value == 6.0f) {
+                    binding.preview.markValue.setTextColor(getColor(R.color.mark_6_0))
+                } else if (mark.Value >= 5.5f) {
+                    binding.preview.markValue.setTextColor(getColor(R.color.mark_5_5))
+                } else if (mark.Value >= 5.0f) {
+                    binding.preview.markValue.setTextColor(getColor(R.color.mark_5_0))
+                } else if (mark.Value >= 4.5f) {
+                    binding.preview.markValue.setTextColor(getColor(R.color.mark_4_5))
+                } else if (mark.Value >= 4.0f) {
+                    binding.preview.markValue.setTextColor(getColor(R.color.mark_4_0))
+                } else {
+                    binding.preview.markValue.setTextColor(getColor(R.color.mark_failing))
+                }
             }
         }
 
